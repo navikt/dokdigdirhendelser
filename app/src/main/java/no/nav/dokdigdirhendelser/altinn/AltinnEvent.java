1a51ef077c5292886b51d9f8710e99f60b7cb6cd
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-04-28 13:24:43 +0200

    MMA-8674 Filtrerer hendelser som blir sendt videre
    * Vi behøver ikke å sende videre hendelser som ikke behandles

--- app/src/main/java/no/nav/dokdigdirhendelser/altinn/AltinnEvent.java
+++ app/src/main/java/no/nav/dokdigdirhendelser/altinn/AltinnEvent.java
@@ -21,23 +21,12 @@ public record AltinnEvent(
 
 	public static final String VALIDATE_SUBSCRIPTION_EVENT_TYPE = "platform.events.validatesubscription";
 	public static final Set<String> CORRESPONDENCE_EVENT_TYPES = Set.of(
-			"no.altinn.correspondence.attachmentinitialized",
-			"no.altinn.correspondence.attachmentuploadprocessing",
-			"no.altinn.correspondence.attachmentpublished",
-			"no.altinn.correspondence.attachmentuploadfailed",
-			"no.altinn.correspondence.attachmentpurged",
-			"no.altinn.correspondence.correspondenceinitialized",
-			"no.altinn.correspondence.correspondencearchived",
-			"no.altinn.correspondence.correspondencepurged",
 			"no.altinn.correspondence.correspondencepublishfailed",
 			"no.altinn.correspondence.correspondencereceiverread",
 			"no.altinn.correspondence.correspondencereceiverconfirmed",
-			"no.altinn.correspondence.correspondencereceiverreserved",
 			"no.altinn.correspondence.correspondencenotificationcreationfailed",
 			"no.altinn.correspondence.correspondencenotificationfailed",
-			"no.altinn.correspondence.correspondencepublished",
-			"no.altinn.correspondence.correspondencereceiverneverread",
-			"no.altinn.correspondence.correspondencereceiverneverconfirmed"
+			"no.altinn.correspondence.correspondencepublished"
 	);
 
 }
